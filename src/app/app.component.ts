dea5e63a790642ffbbc55ea110f73f77b88fea58
berat <cedinckan@gmail.com>
2020-09-10 15:56:51 +0300

    slider and gauge added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'slider';
+  gaugeType='arch';
+  gaugeValue:number;
+  gaugeLabel='motor cikisi';
+  gaugeAppendText="mhz"
+  gaugeMin=0;
+  gaugeMax=100
+  gaugeSize=200;
+  oldMax=this.gaugeMax;
+  oldMin=this.gaugeMin;
+  oldRange=(this.oldMax-this.oldMin);
+
+
+  //newMax=1613
+  //newMin=0
+  newMax=50;
+  newMin=0;
+  newRange=(this.newMax-this.newMin);
+  pressValue($evet){
+    console.log($evet)
+    if(this.gaugeValue>this.gaugeMax){
+      this.gaugeValue= this.gaugeMax
+    }
+    else if(this.gaugeValue<this.gaugeMin){
+      this.gaugeValue= this.gaugeMin;
+    }
+  }
+  /**
+   * formula of scaling
+   * oldRange=(oldMax-oldMin)
+   * newRange=(newMax-newMin)
+   * newValue=(((oldValue-oldMin)*newRange)/oldRange)+newMin
+   */
+  sendData(){
+    if(!(this.gaugeValue > this.gaugeMax && this.gaugeValue<this.gaugeMin)){
+      const newValue=(((this.gaugeValue-this.oldMin)*this.newRange)/this.oldRange)+this.newMin;
+      console.log(newValue)
+
+    }
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatInputModule,} from '@angular/material/input';
+import {NgxGaugeModule} from "ngx-gauge"
+import { ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    NgxGaugeModule,
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
